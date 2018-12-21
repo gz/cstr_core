9a1f8d789e938e2cdff9e592d578a59e9c052222
Gerd Zellweger <mail@gerdzellweger.com>
2018-12-20 19:50:52 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,6 @@ extern crate std;
 #[cfg(feature = "alloc")]
 extern crate alloc;
 extern crate cty;
-extern crate memchr;
 
 #[cfg(feature = "alloc")]
 use alloc::sync::Arc;
@@ -42,6 +41,37 @@ use core::str::{self, Utf8Error};
 
 /// Re-export c_char
 pub use cty::c_char;
+
+mod memchr {
+    #![allow(dead_code)]
+
+    pub fn memchr(n1: u8, haystack: &[u8]) -> Option<usize> {
+        haystack.iter().position(|&b| b == n1)
+    }
+
+    pub fn memchr2(n1: u8, n2: u8, haystack: &[u8]) -> Option<usize> {
+        haystack.iter().position(|&b| b == n1 || b == n2)
+    }
+
+    pub fn memchr3(n1: u8, n2: u8, n3: u8, haystack: &[u8]) -> Option<usize> {
+        haystack.iter().position(|&b| b == n1 || b == n2 || b == n3)
+    }
+
+    pub fn memrchr(n1: u8, haystack: &[u8]) -> Option<usize> {
+        haystack.iter().rposition(|&b| b == n1)
+    }
+
+    pub fn memrchr2(n1: u8, n2: u8, haystack: &[u8]) -> Option<usize> {
+        haystack.iter().rposition(|&b| b == n1 || b == n2)
+    }
+
+    pub fn memrchr3(n1: u8, n2: u8, n3: u8, haystack: &[u8]) -> Option<usize> {
+        haystack
+            .iter()
+            .rposition(|&b| b == n1 || b == n2 || b == n3)
+    }
+
+}
 
 #[inline]
 unsafe fn strlen(p: *const c_char) -> usize {
